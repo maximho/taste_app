7e37574ee264611ae8df5350b2739cd6ff2fd6e8
Maximilianoho <maximho20@gmail.com>
2015-09-28 19:25:29 +0200

    provide and yield commands in views

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -10,13 +10,13 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get help" do
     get :help
     assert_response :success
-    assert_select "title", "Home | Ruby on Rails Taste App"
+    assert_select "title", "Help | Ruby on Rails Taste App"
   end
 
   test "should get about" do
     get :about
     assert_response :success
-    assert_select "title", "Home | Ruby on Rails Taste App"
+    assert_select "title", "About | Ruby on Rails Taste App"
   end
 
 end
